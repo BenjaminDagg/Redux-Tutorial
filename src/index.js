f370691f8071c105f7971e6ad38aa0350769e381
BenjaminDagg <bdagg@csumb.edu>
2018-06-05 18:14:18 -0700

    changed index to implement redux Provider

--- src/index.js
+++ src/index.js
@@ -4,6 +4,18 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import index from './js/index';
+import { Provider } from 'react-redux';
+import store from './store/index';
 
+/*
 ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
+*/
+
+ReactDOM.render(
+  <Provider store={store}>
+      <App/>
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import store from '../store/index';
 import { addTodo } from "../actions";
 
+
 window.store = store;
 window.addTodo = addTodo;
